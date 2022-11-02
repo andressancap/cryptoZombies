bafafe9285347d7feb83d73583618b75225d600b
andsandev <116159555+andressancap@users.noreply.github.com>
2022-11-02 17:04:04 +0100

    Add mapping msg.sender
    Add mapping to know owner of a zombie.
    
    Only allow to create a zombie by user.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.19;
 
 contract ZombieFactory {
-    
+
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
@@ -14,17 +14,24 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        emit NewZombie(id, _name, _dna);
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
