83de69e53985bbaaeae21ce515975d6bdeae1f24
andsandev <116159555+andressancap@users.noreply.github.com>
2022-11-03 10:24:43 +0100

    Add setter kitty address only owner

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -19,16 +19,17 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  // Modifica la definición de la función aquí:
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
+
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // Añade una sentencia if
     if (keccak256(_species) == keccak256("kitty")) {
       newDna = newDna - newDna % 100 + 99;
     }
@@ -38,7 +39,6 @@ contract ZombieFeeding is ZombieFactory {
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // Y modifica la llamada de la función aquí:
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
